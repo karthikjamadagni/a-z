f7d8dc8a2236b13ec5eda8adb4da5d4e7bf11699
Karthik Jamadagni <karthikng44gmail.com>
2026-05-03 21:56:36 +0530

    Sorting completed

---
+++ Sorting/Part One/SelectionSort.py
@@ -0,0 +1,14 @@
+class Solution: 
+    def selectionSort(self, arr):
+        #code here
+        n = len(arr)
+        
+        for i in range(n-1):
+            min_index = i
+            for j in range(i+1, n):
+                if arr[j] < arr[min_index]:
+                    min_index = j
+                    
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+            
+        
---
+++ Sorting/Part Two/MergeSort.py
@@ -0,0 +1,47 @@
+class Solution:
+ 
+    def mergeSort(self, arr, l, r):
+        #code here
+        if l < r:
+            m = l + (r-l)//2
+            self.mergeSort(arr, l, m)
+            self.mergeSort(arr, m+1, r)
+            self.merge(arr, l, m, r)
+            
+            
+    def merge(self, arr, l, m, r):
+        n1 = m - l + 1
+        n2 = r - m
+        
+        L = [0] * n1
+        R = [0] * n2
+        
+        for i in range(n1):
+            L[i] = arr[l+i]
+            
+        for i in range(n2):
+            R[i] = arr[m+1+i]
+            
+            
+        i = j = 0
+        k = l
+    
+        while i < n1 and j < n2:
+            if L[i] <= R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+    
+        while i < n1:
+            arr[k] = L[i]
+            i += 1
+            k += 1
+        while j < n2:
+            arr[k] = R[j]
+            j += 1
+            k += 1
+            
+        
